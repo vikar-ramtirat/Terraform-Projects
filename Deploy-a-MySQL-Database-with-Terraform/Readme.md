23e2aa1c21cf8889a1e280a20ee3a26024ed2f52
vikar-ramtirat <vikarramtirat@outlook.com>
2024-08-06 23:22:13 +0100

    Added Deploy a MySQL Database with Terraform

--- Deploy-a-MySQL-Database-with-Terraform/Readme.md
+++ Deploy-a-MySQL-Database-with-Terraform/Readme.md
@@ -1,7 +1,9 @@
-# Deploy a virtual network and two subnets
+# Deploy a MySQL database
+
+# Create a MySQL v5.7 database instance with 5GB of storage with a Gen 5, basic tier database SKU. Give it 5Gb of storage, set an admin password, and create a database.
 
 1st Step Run cmd - terraform inti to initializes a working directory and downloads the necessary provider plugins and modules and setting up the backend for storing your infrastructure's state.
 
 2nd step - Run terraform plan and review the output to confirm that Terraform will create the desired resource. Green plus signs will indicate the resources that need to be added.
 
-3rd Step - Run terraform apply, answering yes to the prompt to continue to deploy vnet configuration with subnets
+3rd Step - Run terraform apply, answering yes to the prompt to continue to deploy db with configuration.
---
+++ Deploy-a-MySQL-Database-with-Terraform/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+}
+
+resource "azurerm_mysql_server" "example" {
+  name                = "tflab-mysqlserver-1-app01"
+  location            = "East US"
+  resource_group_name = "186-599a5fe3-deploy-a-mysql-database-with-terrafor"
+
+  sku_name = "B_Gen5_2"
+  # capacity = "2"
+  # tier = "Basic"
+  # family = "Gen4"
+        
+  storage_mb = "5120"
+  version = "8.0"
+  backup_retention_days = "7"
+  ssl_enforcement_enabled = true
+  infrastructure_encryption_enabled = false
+  auto_grow_enabled = true
+  public_network_access_enabled = true 
+
+  administrator_login          = "mysqladminun"
+  administrator_login_password = "easytologin4once!"
+}
+
+resource "azurerm_mysql_database" "example" {
+  name                = "appdb01"
+  resource_group_name = "186-599a5fe3-deploy-a-mysql-database-with-terrafor"
+  server_name         = azurerm_mysql_server.example.name
+  charset             = "utf8"
+  collation           = "utf8_unicode_ci"
+}
